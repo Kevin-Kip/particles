1540c2aa43e8d27a109e474df1810ddd67d5a94e
Kevin-Kip <masterfork5@gmail.com>
2018-07-22 14:51:20 +0300

    Added circular motion

---
+++ README.md
@@ -0,0 +1,8 @@
+## Circular Motion
+Circular Motion implementation in HTML5 Canvas and JavaScript.
+
+Try it out [here]().
+
+### Look and feel
+<img src="screenshots/screenshot1.png" width="350px"/>
+<img src="screenshots/screenshot.png" width="350px"/>
--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 var canvas = document.querySelector('canvas');
-var context = canvas.getContext('2d');
+var c = canvas.getContext('2d');
 
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
-window.addEventListener('resize', () => {
+window.addEventListener('resize', function () {
    canvas.height = window.innerHeight;
    canvas.width = window.innerWidth;
 });
@@ -26,3 +26,55 @@ window.addEventListener('mousemove', function (event) {
   mouse.x = event.x;
   mouse.y = event.y;
 });
+
+function randomDistance(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function Particle(x, y, radius, color){
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.radians = Math.random() * Math.PI * 2;
+    this.velocity = 0.03;
+    this.distance = randomDistance(30, 300);
+    var previous;
+
+    this.update = function () {
+        previous = {x: this.x, y:this.y};
+        this.radians += this.velocity;
+        this.x = x + Math.cos(this.radians) * this.distance;
+        this.y = y + Math.sin(this.radians) * this.distance;
+        this.draw();
+    };
+
+    this.draw = function() {
+        c.beginPath();
+        c.strokeStyle = this.color;
+        c.lineWidth = this.radius;
+        c.moveTo(previous.x, previous.y);
+        c.lineTo(this.x, this.y);
+        c.stroke();
+        c.closePath();
+    };
+}
+var particleArray = [];
+for (var i = 0; i < 200; i++){
+    var x = canvas.width / 2;
+    var y = canvas.height / 2;
+    var radius = (Math.random() * 2) + 3;
+    var color = colorArray[Math.floor(Math.random() * colorArray.length)];
+    particleArray.push(new Particle(x, y, radius, color));
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    c.fillStyle = 'rgba(255, 255, 255, 0.05)';
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    for (var j = 0; j < particleArray.length; j++){
+        particleArray[j].update();
+    }
+}
+
+animate();
